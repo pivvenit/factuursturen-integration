657a8f50c5c274f2a8ae20f321a1a423e96dcb9c
Sander Klock <680327+Qrious@users.noreply.github.com>
2022-10-30 16:03:12 +0100

    Fix issue on sundays when the prices are 0

--- src/RequestData/InvoiceLine.php
+++ src/RequestData/InvoiceLine.php
@@ -212,6 +212,8 @@ class InvoiceLine implements InvoiceLineInterface
             'dicount_pct' => $this->getDiscountPct(),
         );
 
-        return $filter_nulls ? array_filter($array) : null;
+        return $filter_nulls ? array_filter(function ($item) {
+			return !is_null($item);
+		}, $array) : null;
     }
 }
